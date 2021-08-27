2f9db5a27cb209a86cf25acecfb7273b48eb126f
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-27 19:13:04 +0300

    Solution for 6_1

--- unicafe-redux/src/reducer.test.js
+++ unicafe-redux/src/reducer.test.js
@@ -32,4 +32,64 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero action zeros the state', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = { good: 123, ok: 123, bad: 123 }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('Invalid action does nothing', () => {
+    const action = {
+      type: 'INVALID_ACTION'
+    }
+    const state = { good: 123, ok: 312, bad: 444 }
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 123,
+      ok: 312,
+      bad: 444
+    })
+  })
 })

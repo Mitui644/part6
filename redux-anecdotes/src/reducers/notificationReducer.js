ad54e201b4f29ac340c9bfa3f3010478ccb5437a
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-28 12:10:34 +0300

    Solution for 6_10

---
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,9 @@
+const notificationReducer = (state = 'initial notification', action) => {
+    console.log(action)
+    switch (action.type) {
+      default:
+        return state
+    }
+  }
+
+export default notificationReducer

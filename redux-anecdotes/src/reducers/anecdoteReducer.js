411a1f31eccd94c852001d6d109cbba8eb8fef91
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-28 13:35:56 +0300

    Solution for 6_13

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,30 +1,11 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 const getId = () => (100000 * Math.random()).toFixed(0)
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
+const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
   switch(action.type) {
-    case 'VOTE':
+    case 'VOTE_ANECDOTE':
       const id = action.data.id
       const anecdote = state.find(n => n.id === id)
       const changedAnecdote = { 
@@ -32,8 +13,10 @@ const anecdoteReducer = (state = initialState, action) => {
         votes: anecdote.votes + 1
       }
       return state.map(a => a.id !== id ? a : changedAnecdote )
-    case 'CREATE':
+    case 'CREATE_ANECDOTE':
       return [...state, action.data]
+    case 'SET_ANECDOTES':
+      return action.data
     default:
         return state
   }
@@ -41,19 +24,26 @@ const anecdoteReducer = (state = initialState, action) => {
 
 export const addVote = (id) => {
   return {
-    type: 'VOTE',
+    type: 'VOTE_ANECDOTE',
     data: { id }
   }
 }
 
 export const addAnecdote = (content) => {
   return {
-    type: 'CREATE',
+    type: 'CREATE_ANECDOTE',
     data: {
       content,
       id: getId(),
       votes: 0
     }
+  }
+}
+
+export const setAnecdotes = (anecdotes) => {
+  return {
+    type: 'SET_ANECDOTES',
+    data: anecdotes
   }
 }
 
